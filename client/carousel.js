69d61e03b6362fca5f54d8debf9416412d4e8a28
Fabrikh <62346654+Fabrikh@users.noreply.github.com>
2021-05-07 20:36:13 +0200

    Embedded html

--- client/carousel.js
+++ client/carousel.js
@@ -2,11 +2,11 @@ var app = new Vue ({
     el : '#innercar',
     data : {
         carousel: {
-            First   :{id : "/client/assets/img/texts/Evento_1.txt", class : "carousel-item active", src : "assets/img/images/Evento_1.jpg", alt : "Primo"},
+            First   :{id : "/client/assets/img/texts/Evento_1.html", class : "carousel-item active", src : "assets/img/images/Evento_1.jpg", alt : "Primo"},
 
-            Second  :{id : "/client/assets/img/texts/Evento_2.txt", class : "carousel-item"       , src : "assets/img/images/Evento_2.jpg", alt : "Secondo"},
+            Second  :{id : "/client/assets/img/texts/Evento_2.html", class : "carousel-item"       , src : "assets/img/images/Evento_2.jpg", alt : "Secondo"},
 
-            Third   :{id : "/client/assets/img/texts/Evento_3.txt", class : "carousel-item"       , src : "assets/img/images/Evento_3.jpg", alt : "Terzo"}  
+            Third   :{id : "/client/assets/img/texts/Evento_3.html", class : "carousel-item"       , src : "assets/img/images/Evento_3.jpg", alt : "Terzo"}  
         }
     }
 });
