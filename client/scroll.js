0bfd25974523537b6cd3cafecdcd276f89be2745
Antonello <antonellomele94@gmail.com>
2021-05-14 19:27:42 +0200

    conflict resolved

---
+++ client/scroll.js
@@ -0,0 +1,11 @@
+$(".scrollup").click(function (e) {
+  e.preventDefault();
+  var goTo = this.getAttribute("href");
+
+  var interval = setInterval(function () {
+    if (!$("main").hasClass("nav-is-visible")) {
+      clearInterval(interval);
+      window.location = goTo;
+    }
+  }, 100);
+});
